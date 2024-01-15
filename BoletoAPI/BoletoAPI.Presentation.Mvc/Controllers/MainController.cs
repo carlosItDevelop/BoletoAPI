527df0507218304b51d61082278a9a1d083cc221
Carlos A Santos <carlos.itdeveloper@gmail.com>
2024-01-15 19:09:22 -0300

    MainController and MvcBoleto Added in Mvc Project

---
+++ BoletoAPI/BoletoAPI.Presentation.Mvc/Controllers/MainController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SDTEC.GestorEducacional.Models;
+
+namespace SDTEC.GestorEducacional.BoletoMvc.Controllers
+{
+    [ApiController]
+    public class MainController : Controller
+    {
+        protected bool ResponsePossuiErros(ResponseResult resposta)
+        {
+            if (resposta != null && resposta.Errors != null && resposta.Errors.Mensagens.Any())
+            {
+                foreach (var mensagem in resposta.Errors.Mensagens)
+                {
+                    ModelState.AddModelError(string.Empty, mensagem);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        [Route("adicionarerrovalidacao")]
+        public void AdicionarErroValidacao(string mensagem)
+        {
+            ModelState.AddModelError(string.Empty, mensagem);
+        }
+
+        [Route("operacaoinvalida")]
+        protected bool OperacaoValida()
+        {
+            return ModelState.ErrorCount == 0;
+        }
+    }
+}
---
+++ BoletoAPI/BoletoAPI.Presentation.Mvc/Controllers/MvcBoletoController.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using SDTEC.GestorEducacional.BoletoMvc.Services.Boleto;
+using SDTEC.GestorEducacional.BoletoMvc.Models;
+
+namespace SDTEC.GestorEducacional.BoletoMvc.Controllers
+{
+    public class MvcBoletoController : MainController
+    {
+        private readonly IBoletoService _boletoService;
+
+        public MvcBoletoController(IBoletoService boletoService)
+        {
+            _boletoService = boletoService;
+        }
+
+        [HttpGet("gerar-boleto")]
+        public IActionResult AdicionarCriador()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("gerar-boleto")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AdicionarCriador([FromForm] BoletoViewModel viewModel)
+        {
+
+            //viewModel.Id = // gerar Id
+
+            if (!ModelState.IsValid)
+            {
+                TempData ["Erros"] =
+                    ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)).ToList();
+                return View(viewModel);
+            }
+
+            if (!OperacaoValida())
+                return View(viewModel);
+
+
+            if (ResponsePossuiErros(await _boletoService.GerarBoletoService(viewModel)))
+            {
+                TempData ["Erros"] =
+                    ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)).ToList();
+
+                return View(viewModel);
+            }
+
+            return RedirectToAction("Index", "MvcBoleto");
+
+        }
+
+    }
+}
